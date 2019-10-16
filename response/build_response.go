5af0f9a414ffd3426547dfbc52823597b033ce30
ywardhana <muhammad.yasher@gmail.com>
2019-10-16 16:25:05 +0700

    adding response

---
+++ response/build_response.go
@@ -0,0 +1,42 @@
+package response
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Response struct {
+	Data    interface{} `json:"data,omitempty"`
+	Message string      `json:"message,omitempty"`
+	Meta    MetaInfo    `json:"meta"`
+}
+
+type MetaInfo struct {
+	HttpStatus int `json:"http_status"`
+	Offset     int `json:"offset,omitempty"`
+	Limit      int `json:"limit,omitempty"`
+	Total      int `json:"total,omitempty"`
+}
+
+func Write(w http.ResponseWriter, response Response) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(response.Meta.HttpStatus)
+	json.NewEncoder(w).Encode(&response)
+}
+
+func OKWithMeta(w http.ResponseWriter, data interface{}, message string, meta MetaInfo) {
+	meta.HttpStatus = 200
+	response := Response{
+		Data:    data,
+		Message: message,
+		Meta:    meta,
+	}
+	Write(w, response)
+}
+
+func OK(w http.ResponseWriter, data interface{}, message string) {
+	meta := MetaInfo{
+		HttpStatus: 200,
+	}
+	OKWithMeta(w, data, message, meta)
+}
